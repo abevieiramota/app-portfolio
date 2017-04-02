f6eeffb200ad51901d168a33e95c1450dab68d3d
Abelardo Vieira Mota <abevieiramota@gmail.com>
2017-04-02 19:34:26 -0300

    adiciona rota para fotos/new e otherwise

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,9 +1,17 @@
 angular.module("portfolio", [ "minhasDiretivas", "ngAnimate", "ngRoute" ])
-.config(function($routeProvider) {
-	
-	$routeProvider.when("/fotos", {
-		templateUrl: "partials/principal.html",
-		controller: "FotosController"
-	});		
-	
-});
+		.config(function($routeProvider) {
+
+			$routeProvider.when("/fotos", {
+				templateUrl : "partials/principal.html",
+				controller : "FotosController"
+			});
+
+			$routeProvider.when("/fotos/new", {
+				templateUrl : "partials/fotos.html"
+			});
+
+			$routeProvider.otherwise({
+				redirectTo : "/fotos"
+			});
+
+		});
