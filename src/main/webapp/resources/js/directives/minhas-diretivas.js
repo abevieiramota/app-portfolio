eb82f29f0daf5934f511445fb8223bb93736189a
Abelardo Vieira Mota <abevieiramota@gmail.com>
2017-04-02 19:33:40 -0300

    adiciona directive para renderizar foto

--- src/main/webapp/resources/js/directives/minhas-diretivas.js
+++ src/main/webapp/resources/js/directives/minhas-diretivas.js
@@ -1,20 +1,30 @@
-angular
-		.module("minhasDiretivas", [])
-		.directive(
-				"meuPainel",
-				function() {
+angular.module("minhasDiretivas", []).directive("meuPainel", function() {
 
-					var ddo = {};
+	var ddo = {};
 
-					ddo.restrict = "AE";
+	ddo.restrict = "AE";
 
-					ddo.scope = {
-						titulo : "@"
-					};
-					// mantenha os elementos filhos
-					ddo.transclude = true;
+	ddo.scope = {
+		titulo : "@"
+	};
+	// mantenha os elementos filhos
+	ddo.transclude = true;
 
-					ddo.templateUrl = "resources/js/directives/meu-painel.html";
+	ddo.templateUrl = "resources/js/directives/meu-painel.html";
 
-					return ddo;
-				});
+	return ddo;
+}).directive("minhaFoto", function() {
+	
+	var ddo = {};
+	
+	ddo.restrict = "AE";
+	
+	ddo.scope = {
+			url: "@",
+			titulo: "@"
+	};
+	
+	ddo.templateUrl = "resources/js/directives/minha-foto.html";
+	
+	return ddo;
+});
